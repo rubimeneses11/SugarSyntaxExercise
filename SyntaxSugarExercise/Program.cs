c575f0c3365729b0b98a70e52baa8c3d3b193b47
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-11 13:25:30 -0500

    syntax sugar done

--- SyntaxSugarExercise/Program.cs
+++ SyntaxSugarExercise/Program.cs
@@ -6,7 +6,8 @@ namespace SyntaxSugarExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
 }
